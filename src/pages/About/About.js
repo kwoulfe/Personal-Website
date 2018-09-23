f99757379679a6ecf620f951a658b2813a64a704
Kevin Woulfe <kevinpwoulfe@gmail.com>
2018-09-22 17:51:36 -0700

    ABout Me changes

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -14,9 +14,17 @@ const About = () => (
           Full Stack Web Developer • Sacramento, California
           <br />
           <br />
+          About a year go I decided to change careers from Bicycle Retail to Web
+          Development. It's been one of the most rewarding decisions of my life.
+          I once heard someone describe coding as "fireworks for your brain,"
+          and I couldn't agree more. I'm certainly not afraid of a challenge and
+          for that reason alone, I know I'm in the right field. I love the
+          problem solving and attention to detail that Web Development requires
+          and take great joy in creating clean, workable sites with crisp and
+          concise code.
+          <br />
           When I'm not coding, you'll find me riding my bike, hiking, camping,
-          or building/fixing anything I can get my hands on. I also love
-          baseball, watching movies, and eating good food.
+          or building/fixing anything I can get my hands on.
           <br />
           My wife and I love to travel. All the lovely greenery photographed on
           this site is from a trip we recently took to Ireland. I can't wait to
